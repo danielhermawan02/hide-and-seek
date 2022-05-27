9e9708291e204a79850349f8e25641c7b36f7360
PierraMuhammad <91307555+PierraMuhammad@users.noreply.github.com>
2022-05-27 23:32:43 +0700

    save1

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+Getting pathTraceback (most recent call last):
+#   File "c:\Users\LENOVO\OneDrive\Desktop\VSCO\Python\KB_Hide & Seek\Artificial-Intelligence\07_packmanAStar.py", line 475, in <module>     
+#     main()
+#   File "c:\Users\LENOVO\OneDrive\Desktop\VSCO\Python\KB_Hide & Seek\Artificial-Intelligence\07_packmanAStar.py", line 465, in main
+#     getPathBacktracking()
+#   File "c:\Users\LENOVO\OneDrive\Desktop\VSCO\Python\KB_Hide & Seek\Artificial-Intelligence\07_packmanAStar.py", line 417, in getPathBacktracking
+#     print(parent[currPoint[0]][currPoint[1]])
+# IndexError: list index out of range
